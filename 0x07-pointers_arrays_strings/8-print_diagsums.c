25b88121a9bd20511c99dc0a3352d7280369499b
Ropafadzo Manyuchi <ropafadzomanyuchi@gmail.com>
2023-03-07 22:17:34 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,30 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
+#include "main.h"                                                                                                                  
+#include <stdio.h>                                                                                                                 
+                                                                                                                                   
+/**                                                                                                                                
+ * print_diagsums - Prints the sum of the two diagonals                                                                            
+ *                  of a square matrix of integers.                                                                                
+ * @a: The matrix of integers.                                                                                                     
+ * @size: The size of the matrix.                                                                                                  
  */
-int main(void)
-{
-    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 3);
-    print_diagsums((int *)c5, 5);
-    return (0);
+void print_diagsums(int *a, int size)                                                                                              
+{                                                                                                                                  
+        int index, sum1 = 0, sum2 = 0;                                                                                             
+                                                                                                                                   
+        for (index = 0; index < size; index++)                                                                                     
+        {                                                                                                                          
+                sum1 += a[index];                                                                                                  
+                a += size;                                                                                                         
+        }                                                                                                                          
+                                                                                                                                   
+        a -= size;                                                                                                                 
+                                                                                                                                   
+        for (index = 0; index < size; index++)                                                                                     
+        {                                                                                                                          
+                sum2 += a[index];                                                                                                  
+                a -= size;                                                                                                         
+        }                                                                                                                          
+                                                                                                                                   
+        printf("%d, %d\n", sum1, sum2);                                                                                            
 }
+
